b1aebb03abc6076ea444a02dde1a2afc731d8c68
Daniel Bauer <daniel.bauer@senckenberg.de>
2025-05-11 18:23:18 +0200

    feat: io should allow files as input

--- src/loop_modeller/io.py
+++ src/loop_modeller/io.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from biotite.sequence import ProteinSequence
 from biotite.structure import AtomArray, AtomArrayStack
 from biotite.structure.io import pdbx
 from biotite.database import rcsb
 
-def load_structure(pdb_id: str) -> AtomArrayStack:
+def load_structure(pdb_id_or_path: str|Path) -> AtomArrayStack:
     """
     Load a protein structure from the RCSB PDB database.
 
@@ -17,10 +18,15 @@ def load_structure(pdb_id: str) -> AtomArrayStack:
     AtomArrayStack
         The loaded structure as an AtomArrayStack object.
     """
-    # Fetch the PDB file from the RCSB PDB database and load it
-    pdb_file = rcsb.fetch(pdb_id, format="mmcif", target_path="structures")
-    file = pdbx.CIFFile.read(pdb_file)
-    structure = pdbx.get_structure(file)
+    if Path(pdb_id_or_path).is_file():
+        # If a file path is provided, load the structure from the file
+        file = pdbx.CIFFile.read(pdb_id_or_path)
+        structure = pdbx.get_structure(file)
+    else:
+        # Fetch the PDB file from the RCSB PDB database and load it
+        pdb_file = rcsb.fetch(pdb_id_or_path, format="mmcif", target_path="structures")
+        file = pdbx.CIFFile.read(pdb_file)
+        structure = pdbx.get_structure(file)
 
     # If the structure is a single AtomArray, convert it to an AtomArrayStack
     if isinstance(structure, AtomArray):
@@ -28,7 +34,7 @@ def load_structure(pdb_id: str) -> AtomArrayStack:
 
     return structure
 
-def load_sequence(pdb_id: str) -> dict[str, ProteinSequence]:
+def load_sequence(pdb_id_or_path: str|Path) -> dict[str, ProteinSequence]:
     """
     Load the sequence of a protein from the RCSB PDB database.
 
@@ -42,7 +48,14 @@ def load_sequence(pdb_id: str) -> dict[str, ProteinSequence]:
     str
         The sequence of the protein structure.
     """
-    # Fetch the PDB file from the RCSB PDB database and load it
-    pdb_file = rcsb.fetch(pdb_id, format="mmcif", target_path="structures")
-    file = pdbx.CIFFile.read(pdb_file)
+
+    if Path(pdb_id_or_path).is_file():
+        # If a file path is provided, load the sequence from the file
+        file = pdbx.CIFFile.read(pdb_id_or_path)
+        return pdbx.get_sequence(file)
+        file = pdbx.CIFFile.read(pdb_id_or_path)
+    else:
+        # Fetch the PDB file from the RCSB PDB database and load it
+        pdb_file = rcsb.fetch(pdb_id_or_path, format="mmcif", target_path="structures")
+        file = pdbx.CIFFile.read(pdb_file)
     return pdbx.get_sequence(file)
